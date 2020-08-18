dd6c5edce01c92364ee205e364a4beeac186bef2
ajes <ajescode@adamspychala.pl>
2020-08-18 03:57:59 +0200

    DM test script updated

--- simulation/script/db_test/db_test.py
+++ simulation/script/db_test/db_test.py
@@ -78,9 +78,9 @@ def dm_test(actual_lst, pred1_lst, pred2_lst, h=1, crit="MSE", power=2):
             return True
 
         for actual, pred1, pred2 in zip(actual_lst, pred1_lst, pred2_lst):
-            is_actual_ok = compiled_regex(str(abs(float(actual))))
-            is_pred1_ok = compiled_regex(str(abs(float(pred1))))
-            is_pred2_ok = compiled_regex(str(abs(float(pred2))))
+            is_actual_ok = compiled_regex(str(abs(actual)))
+            is_pred1_ok = compiled_regex(str(abs(pred1)))
+            is_pred2_ok = compiled_regex(str(abs(pred2)))
             if (not (is_actual_ok and is_pred1_ok and is_pred2_ok)):
                 msg = "An element in the actual_lst, pred1_lst or pred2_lst is not numeric."
                 rt = -1
